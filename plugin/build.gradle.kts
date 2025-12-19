b0654ddcd2453f57fb01f95474578003d0e145f9
Gustavo Paredes <66433837+CroaBeast@users.noreply.github.com>
2025-12-19 09:37:59 -0500

    Fixing compiling

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,33 @@
+val coreProject = project(":core")
+val coreMainOutput = coreProject.extensions.getByType<SourceSetContainer>()["main"].output
+
 dependencies {
-    implementation(project(":core"))
+    implementation(coreProject)
+}
+
+val moduleProjects = rootProject.subprojects.filter { it.path.startsWith(":module:") }
+val moduleJarTasks = moduleProjects.map { it.tasks.named<Jar>("jar") }
+
+tasks.named<Jar>("jar") {
+    dependsOn(coreProject.tasks.named("classes"))
+    from(coreMainOutput)
+    dependsOn(moduleJarTasks)
+    moduleJarTasks.forEach { jarTask ->
+        from(jarTask.flatMap { it.archiveFile }) {
+            into("modules")
+        }
+    }
+}
+
+tasks.named<Jar>("shadowJar") {
+    dependsOn(coreProject.tasks.named("classes"))
+    from(coreMainOutput)
+    dependsOn(moduleJarTasks)
+    moduleJarTasks.forEach { jarTask ->
+        from(jarTask.flatMap { it.archiveFile }) {
+            into("modules")
+        }
+    }
 }
 
 tasks.processResources {
